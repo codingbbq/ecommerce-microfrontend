829e0711f27ec678ca4048ad0872e6cf513efde1
codingbbq <codingbbq@gmail.com>
2025-04-07 20:25:24 +0530

    functional microfrontend

--- cart/src/bootstrap.js
+++ cart/src/bootstrap.js
@@ -1,5 +1,15 @@
 import faker from 'faker';
 
-const message = `<div>You have ${faker.random.number()} items in your cart</div>`;
+const mount = (el) => {
+	const message = `<div>You have ${faker.random.number()} items in your cart</div>`;
+	el.innerHTML = message;
+};
 
-document.querySelector("#cart").innerHTML = message;
+if (process.env.NODE_ENV === 'development') {
+	const el = document.querySelector('#dev-cart');
+	if (el) {
+		// We are probably running in isolation
+		mount(el);
+	}
+}
+export { mount };
